a3575bdc41658f5573cadc649474bb75c640f639
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2025-05-07 00:11:46 +0200

    update

--- worker.js
+++ worker.js
@@ -1,13 +1,30 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+import { Client } from '@cloudflare/pg';
 
-addEventListener('fetch', event => {
-  event.respondWith(handleEvent(event))
-})
+export default {
+  async fetch(request, env) {
+    const client = new Client({
+      connectionString: env.HYPERDRIVE.connectionString,
+      connection: {
+        application_name: "cabma"
+      }
+    });
 
-async function handleEvent(event) {
-  try {
-    return await getAssetFromKV(event)
-  } catch (e) {
-    return new Response(`Error: ${e.message}`, { status: 500 })
+    try {
+      await client.connect();
+      const query = {
+        text: 'SELECT * FROM users WHERE id = $1',
+        values: [request.params.id]
+      };
+      const res = await client.query(query);
+      
+      return Response.json(res.rows);
+    } catch (err) {
+      return Response.json(
+        { error: err.message },
+        { status: 500 }
+      );
+    } finally {
+      await client.end();
+    }
   }
 }
