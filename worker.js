e64b59fca498dc91ed9f2f71879bad3c7b02c9e6
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2025-05-06 19:14:28 +0200

    update

--- worker.js
+++ worker.js
@@ -1,21 +1,7 @@
 import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
-import { Client } from 'pg'
+import pg from 'pg'
 
-async function queryDatabase(hyperdrive) {
-  const client = new Client({
-    host: hyperdrive.connection.host,
-    port: hyperdrive.connection.port,
-    database: hyperdrive.connection.database,
-    user: hyperdrive.connection.user,
-    password: hyperdrive.connection.password,
-    ssl: true
-  })
-  
-  await client.connect()
-  const res = await client.query('SELECT NOW()')
-  await client.end()
-  return res.rows
-}
+const { Client } = pg
 
 export default {
   async fetch(request, env, ctx) {
@@ -24,12 +10,19 @@ export default {
     // Route API
     if (url.pathname.startsWith('/api')) {
       try {
-        const data = await queryDatabase(env.HYPERDRIVE_DB)
-        return new Response(JSON.stringify(data), {
+        const client = new Client({
+          connectionString: env.HYPERDRIVE_DB.connectionString
+        })
+        
+        await client.connect()
+        const res = await client.query('SELECT NOW()')
+        await client.end()
+        
+        return new Response(JSON.stringify(res.rows), {
           headers: { 'Content-Type': 'application/json' }
         })
       } catch (err) {
-        return new Response(JSON.stringify({ error: err.message }), { 
+        return new Response(JSON.stringify({ error: err.message }), {
           status: 500,
           headers: { 'Content-Type': 'application/json' }
         })
