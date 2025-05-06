6fe90ecf40dc03b8bfc3e74f44ff9d69033f9571
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2025-05-07 00:26:33 +0200

    update

--- worker.js
+++ worker.js
@@ -1,44 +1,24 @@
-import { Client } from 'pg';
+import postgres from 'postgres';
 
 export default {
   async fetch(request, env) {
-    // Vérifiez que la requête est une GET
-    if (request.method !== 'GET') {
-      return new Response('Method not allowed', { status: 405 });
-    }
-
-    const url = new URL(request.url);
-    const id = url.searchParams.get('id');
-
-    if (!id) {
-      return Response.json(
-        { error: "Missing 'id' parameter" },
-        { status: 400 }
-      );
-    }
-
-    const client = new Client({
-      connectionString: env.HYPERDRIVE.connectionString,
-      connection: {
-        application_name: "cabma"
-      }
+    const sql = postgres(env.HYPERDRIVE.connectionString, {
+      ssl: 'require'
     });
 
     try {
-      await client.connect();
-      const res = await client.query({
-        text: 'SELECT * FROM users WHERE id = $1',
-        values: [id]
-      });
+      const [user] = await sql`
+        SELECT * FROM users WHERE id = ${request.params.id}
+      `;
       
-      return Response.json(res.rows[0] || { error: "User not found" });
+      return Response.json(user || { error: "User not found" });
     } catch (err) {
       return Response.json(
         { error: err.message },
         { status: 500 }
       );
     } finally {
-      await client.end();
+      await sql.end();
     }
   }
 }
