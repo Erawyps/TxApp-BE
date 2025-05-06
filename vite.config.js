38e5b60a4bdd740d86fb92e7c2ccfece33fd75f6
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2025-05-06 16:09:06 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import jsconfigPaths from 'vite-jsconfig-paths'
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr'
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths(), svgr(),
-  eslint(), tailwindcss(),cloudflare()],
+  eslint(), tailwindcss()],
 })
