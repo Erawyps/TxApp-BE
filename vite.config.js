0faa08c758d8b15f207cdc26efb3f5447ce9c1c3
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2025-05-07 04:02:52 +0200

    chore: update dependencies and configuration for Cloudflare integration
    - Added @cloudflare/vite-plugin and wrangler as dev dependencies in package.json.
    - Updated vite.config.js to include Cloudflare plugin in the Vite build process.
    - Removed obsolete worker.js file and wrangler.toml configuration.
    - Created new .wrangler/deploy/config.json for deployment settings.
    - Added wrangler.jsonc for Cloudflare worker configuration with updated settings.

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import jsconfigPaths from 'vite-jsconfig-paths'
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr'
 import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), jsconfigPaths(), svgr(),
+  plugins: [react(), cloudflare(), jsconfigPaths(), svgr(),
   eslint(), tailwindcss()],
 })
