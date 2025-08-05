6898821b33b7c4d9779663ad486f30766546cc9f
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2025-08-05 15:56:04 +0200

    fix: Update import path for VehicleInfo component to VehiculeInfo for consistency

--- src/app/pages/forms/new-post-form/index.jsx
+++ src/app/pages/forms/new-post-form/index.jsx
@@ -9,7 +9,7 @@ import { shiftSchema } from "./schema";
 import { Page } from "components/shared/Page";
 import { Button } from "components/ui";
 import { DriverInfo } from "./components/DriverInfo";
-import { VehicleInfo } from "./components/VehicleInfo";
+import { VehicleInfo } from "./components/VehiculeInfo";
 import { ServiceHours } from "./components/ServiceHours";
 import { IndexReadings } from "./components/IndexReadings";
 import { CourseForm } from "./components/CourseForm";
